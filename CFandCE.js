f5758a7d970ebf4f86d6b61182826cc6c6409203
cjwsstrm <cjwesstrom@gmail.com>
2017-06-20 20:44:29 +0000

    Adds additional exercises

---
+++ CFandCE.js
@@ -0,0 +1,16 @@
+var theNumber = Number(prompt("Pick a number", ""));
+  if (!isNaN(theNumber))
+    alert("Your number is the square root of " +
+    theNumber * theNumber);
+  else
+    alert("Hey. Why didn't you give me a number?");
+
+
+var num = Number(prompt("Pick a number", "0"));
+
+if (num < 10)
+  alert("Small");
+else if (num < 100)
+  alert("Medium");
+else
+  alert("Large");
---
+++ arraytesting.js
@@ -0,0 +1,13 @@
+var listOfNumbers = [2, 3, 5, 7, 11];
+console.log(listOfNumbers.length);
+var doh = "Doh";
+console.log(doh.toUpperCase);
+console.log(doh.toUpperCase());
+
+var mack = [];
+mack.push("Mack");
+mack.push("the", "Knife");
+console.log(mack);
+console.log(mack.join(" "));
+console.log(mack.pop());
+console.log(mack);
